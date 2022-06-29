1cd8d2e3c89a4f4fa2d173aa08becb1d2441a889
Sabrinalvs <biwneta@gmail.com>
2022-06-29 17:05:08 -0300

    alterações no POST

--- src/controllers/reservas-controllers.js
+++ src/controllers/reservas-controllers.js
@@ -1,15 +1,15 @@
 import Reserva from "../models/reservas-models.js";
-//importar o banco de sqlite-db.js
+import bdSQLite from './infra/sqlite-db.js';
 import ReservaDAO from "../DAO/reservas-dao.js";
 
 const reserva = (app) => {
     //DAO
-    const DadosDAO = new ReservaDAO(BANCO) 
+    const DadosDAO = new ReservaDAO(bdSQLite) 
     //Rota post
     app.post('/reserva', (req, res) => {
         const body = req.body;
         const novaReserva = new Reserva(body.idQuarto, body.idHospede, body.checkin, body.checkout, body.statusPagamento)
-        ReservaDAO.inserirReserva(novaReserva)
+        DadosDAO.inserirReserva(novaReserva)
         .then((result) => {
             res.send("Inserido a reserva com sucesso");
         }).catch((err) => {
