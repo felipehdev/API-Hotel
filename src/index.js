0fe8c7390f8c7d2a460c96d5911da067d6b21499
Sabrinalvs <biwneta@gmail.com>
2022-06-29 16:58:26 -0300

    alterações no index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import express from "express";
 const app = express();
+import bdSQLite from './infra/sqlite-db.js';
 
 app.use(express.json());
+
+import reserva from './controllers/reservas-controllers.js'
+reserva(app, bdSQLite);
 
 app.listen(3003, () => {
   console.log("codando na porta 3003");
