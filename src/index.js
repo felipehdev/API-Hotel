332503e595e73cb4be29674a79224c9b6e978fc0
felipehdev <felipehdev@gmail.com>
2022-06-30 18:42:28 -0300

    adicionado import quarto no index

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ app.use(express.json());
 import reserva from './controllers/reservas-controllers.js'
 reserva(app, bdSQLite);
 
+import quarto from './controllers/quartos-controllers'
+quarto(app, bdSQLite);
+
 app.listen(3003, () => {
   console.log("codando na porta 3003");
 });
