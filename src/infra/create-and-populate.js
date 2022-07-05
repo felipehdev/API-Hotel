72f041f7f0ccc1e8ec645283f2154091e97080bc
felipehdev <felipehdev@gmail.com>
2022-07-05 19:21:48 -0300

    corrigidos bugs na criaçao das tabelas

--- src/infra/create-and-populate.js
+++ src/infra/create-and-populate.js
@@ -26,19 +26,18 @@ CREATE TABLE IF NOT EXISTS "HOSPEDES" (
 
 const ADD_HOSPEDES_DATA = `
 INSERT INTO HOSPEDES (ID_HOSPEDE, NOME, SOBRENOME, CPF, DATA_DE_NASCIMENTO, TELEFONE, EMAIL, ENDEREÇO)
-VALUES 
-
+VALUES
     (1, 'Thomas', 'Cunha', '93223452944', '1987-01-11', '11943238790', 'thomfacilitator@resilia.com', 'Sao Paulo'),
-    (2, 'Kassiane', 'Facanha', '67941379654', '1987-01-11, '11468397865', 'kassifacilitadora@resilia.com', 'Ceara'),
+    (2, 'Kassiane', 'Facanha', '67941379654', '1987-01-11', '11968397865', 'kassifacilitadora@resilia.com', 'Ceara'),
     (3, 'Alexandre', 'Cezar Fh', '12486910988', '1996-07-29', '41986574190', 'alexandre.cezarfh@gmail.com', 'Parana'),
     (4, 'Alexandre', 'Neckel', '82556582590', '1997-01-11', '41996593610', 'aleneckel@gmail.com', 'Parana'),
-    (5, 'André', 'Moura', 88759262370, 1990-01-11, '41926410936', 'andre.moura@gmail.com', 'Parana'),
+    (5, 'André', 'Moura', '88759262370', '1990-01-11', '41926410936', 'andre.moura@gmail.com', 'Parana'),
     (6, 'Carolina', 'Saint', '92334681129', '1997-01-11', '41956285391', 'saint.carol@gmail.com', 'Parana'),
     (7, 'Carolina', 'Villarino', '82773600189', '1997-01-11', '41947635012', 'carol.molly@gmail.com', 'Parana'),
-    (8, 'Diuly', 'Souza', 01892746511, '1997-01-11', '41985672410','diulysouza@gmail.com', 'Parana'),
+    (8, 'Diuly', 'Souza', '01892746511', '1997-01-11', '41985672410', 'diulysouza@gmail.com', 'Parana'),
     (9, 'Eduarda', 'Tybusch', '01836501844', '1997-01-11', '41983567812', 'eduarda.t@gmail.com', 'Parana'),
     (10, 'Eduardo', 'Victor', '12845010983', '1997-01-11', '41967534196', 'eduvictor@gmail.com', 'Parana'),
-    (11, 'Gustavo', 'Arcoverde', '01265846399', '2003-01-11, '41986540027', 'gu.verdin@gmail.com', 'Parana'),
+    (11, 'Gustavo', 'Arcoverde', '01265846399', '2003-01-11', '41986540027', 'gu.verdin@gmail.com', 'Parana'),
     (12, 'Isadora', 'Silva', '02375619833', '2005-01-11', '41986645724', 'silva.isa@gmail.com', 'Parana'),
     (13, 'Jessica', 'Cintra', '46957735448', '1990-01-11', '41988451602', 'jess.cintra@gmail.com', 'Parana'),
     (14, 'Jessica', 'Paiva', '54830211363', '1990-01-11', '41999234025', 'jess.paiva@gmail.com', 'Parana'),
@@ -50,7 +49,7 @@ VALUES
     (20, 'Sabrina', 'Szemberg', '48372991302', '2000-01-11', '41986250183', 'sabrinasz@gmail.com', 'Parana'),
     (21, 'Felipe', 'Honorio', '93348185212', '1992-01-11', '41938250122', 'feliprh@gmail.com', 'Parana'),
     (22, 'Ana Luiza', 'Ferreira', '01472899434', '2003-01-11', '41984724558', 'analuferns@gmail.com', 'Parana'),
-    (23, 'Ana Paula', 'Fedechem', '93472016544', '1996-01-11', '41988542518', 'anapfedechem@gmail.com'),
+    (23, 'Ana Paula', 'Fedechem', '93472016544', '1996-01-11', '41988542518', 'anapfedechem@gmail.com', 'Parana'),
     (24, 'Alessandra', 'Bulka', '32476018327', '2003-01-11', '41993427512', 'alebulka@gmail.com', 'Parana'),
     (25, 'Beatriz', 'Araujo', '02374317333', '2003-01-11', '41982571245', 'bea_araujo@gmail.com', 'Parana'),
     (26, 'Bruna', 'Ravelli', '19475200434', '2000-01-11', '41994128422', 'brunaravelli@gmail.com', 'Parana'),
@@ -59,23 +58,23 @@ VALUES
     (29, 'Gadiel', 'Machado', '74562374192', '2000-01-11', '41992751123', 'gadimachado@gmail.com', 'Parana'),
     (30, 'Giovana', 'Santos', '18137442533', '2003-01-11', '41988251124', 'gihsantos@gmail.com', 'Parana'),
     (31, 'Hellen', 'Costa', '33841049188', '2000-01-11', '41994286557', 'hellen.costa@gmail.com', 'Parana'),
-    (32, 'Isaque', 'Rodrigues', '02318462343', '1992-01-11', '41984157763', 'isaquerod@gmail.com', Parana),
-    (33, 'Joao Vitor', 'Araujo', '84572380811', '2003-01-11', '41993586122', 'joaov.araujo@gmail.com', Parana),
-    (34, 'Thaysa', 'Weber', '11380871905', '1998-01-11', '41988226721', 'thaysaweber@gmail.com', Parana),
-    (35, 'Joao Pedro', 'de Paula', '28334157500', '2000-01-11', '41993461833', 'joaopedrop@gmail.com', Parana),
-    (36, 'Keu', 'Santos', '24281836568', '2000-01-11', '41983125799', 'keusantos@gmail.com', Parana),
-    (37, 'Magali Lima', '73219153044', '1996-01-11', '41988412236', 'maglima@gmail.com', Parana),
+    (32, 'Isaque', 'Rodrigues', '02318462343', '1992-01-11', '41984157763', 'isaquerod@gmail.com', 'Parana'),
+    (33, 'Joao Vitor', 'Araujo', '84572380811', '2003-01-11', '41993586122', 'joaov.araujo@gmail.com', 'Parana'),
+    (34, 'Thaysa', 'Weber', '11380871905', '1998-01-11', '41988226721', 'thaysaweber@gmail.com', 'Parana'),
+    (35, 'Joao Pedro', 'de Paula', '28334157500', '2000-01-11', '41993461833', 'joaopedrop@gmail.com', 'Parana'),
+    (36, 'Keu', 'Santos', '24281836568', '2000-01-11', '41983125799', 'keusantos@gmail.com', 'Parana'),
+    (37, 'Magali', 'Lima', '73219153044', '1996-01-11', '41988412236', 'maglima@gmail.com', 'Parana'),
     (38, 'Mayckel', 'Santos', '33751904093', '2000-01-11', '41998461742', 'santosmayckel@gmail.com', 'Parana'),
     (39, 'Orlando', 'Bernardi', '36510857233', '2003-01-11', '41985173591', 'landinhob@gmail.com', 'Parana'),
     (40, 'Patrick', 'Patchelli', '99357149602', '1992-01-11', '41994221752', 'ppatchelli@hmail.com', 'Parana'),
-    (41, 'Priscila', 'Souza', 75611485392, '1992-01-11', '41994857145', 'prisouza@gmail.com', Parana),
-    (42, 'Raquel', 'Rodrigues', '42475195823', '1998-01-11', '41995764248', 'raquel_rodrigues@gmail.com', Parana),
-    (43, 'Thais', 'Cristina', '46918476341', '1992-01-11', '41952759244', 'cristina.thais@gmail.com', Parana),
-    (44, 'Thaynara', 'Cabral', '12175334511', '2000-01-11', '41984751211', 'thaycabral@gmail.com', Parana),
-    (45, 'Veronica', 'Ribas', '75205712344', '2000-01-11', '41997519472', 'veribas@gmail.com', Parana),
-    (46, 'Victor Gabriel', 'Camargo', '72561085311', '2000-01-11', '41998451084', 'victorg.camargo@gmail.com'),
+    (41, 'Priscila', 'Souza', '75611485392', '1992-01-11', '41994857145', 'prisouza@gmail.com', 'Parana'),
+    (42, 'Raquel', 'Rodrigues', '42475195823', '1998-01-11', '41995764248', 'raquel_rodrigues@gmail.com', 'Parana'),
+    (43, 'Thais', 'Cristina', '46918476341', '1992-01-11', '41952759244', 'cristina.thais@gmail.com', 'Parana'),
+    (44, 'Thaynara', 'Cabral', '12175334511', '2000-01-11', '41984751211', 'thaycabral@gmail.com', 'Parana'),
+    (45, 'Veronica', 'Ribas', '75205712344', '2000-01-11', '41997519472', 'veribas@gmail.com', 'Parana'),
+    (46, 'Victor Gabriel', 'Camargo', '72561085311', '2000-01-11', '41998451084', 'victorg.camargo@gmail.com', 'Parana'),
     (47, 'Rodrigo', 'Junqueria', '58137522354', '1987-01-11', '82997459201', 'rodri_junqueira@gmail.com', 'Alagoas'),
-    (48, 'Maria Celestina', 'de Jesus', '27247184977', '1955-01-11', '27984619400',  'mariac.jesus@gmail.com', 'Espirito Santo'),
+    (48, 'Maria Celestina', 'de Jesus', '27247184977', '1955-01-11', '27984619400', 'mariac.jesus@gmail.com', 'Espirito Santo'),
     (49, 'Orivaldo', 'Almeida', '28411749533', '1982-01-11', '62984717534', 'almeida.o@gmail.com', 'Goias'),
     (50, 'Marlene', 'Oliveira', '29275311745', '1967-01-11', '98921475334', 'olimarlene@gmail.com', 'Maranhao')
     `
@@ -272,8 +271,48 @@ CREATE TABLE IF NOT EXISTS FUNCIONARIOS (
 
 const ADD_FUNCIONARIOS_DATA = `INSERT INTO FUNCIONARIOS (ID_FUNCIONARIO, NOME_FUNCIONARIO, CARGO, STATUS_SERVIÇO, HORARIO_ENTRADA, HORARIO_SAIDA, SALARIO)
 VALUES 
+    (1, "Marcelo Ramos", "Recepcionista", "Ativo", "09:00:00", "17:00:00", 2500),
+    (2, "Alice do Carmo", "Recepcionista", "Ativo", "09:00:00", "17:00:00", 2500),
+    (3, "Brícia da Silva Santos", "Recepcionista", "Ativo", "15:00:00", "23:00:00", 2800),
+    (4, "José Carlos Schneider", "Recepcionista", "Ativo", "07:00:00", "01:00:00", 2800),
+    (5, "Ruan Alberto Vidanova", "Recepcionista", "Férias", "01:00:00", "08:00:00", 2800),
+    (6, "Julia Tereza Pereira", "Recepcionista", "Temporário", "01:00:00", "08:00:00", 2300),
 
-    (1, "Marcelo Ramos", "Recepcionista", "Ativo", "07:00:00", "15:00:00", 2500,00)    
+    (7, "Tony Moreira", "Mecânico", "Ativo", "07:00:00", "16:00:00", 2700),
+    (8, "Carlos Santos Menezes", "Mecânico", "Ativo", "16:00:00", "22:00:00", 2400),
+
+    (9, "Alessandra Santini", "Camareira", "Ativo", "07:00:00", "15:00:00", 2900),
+    (10, "Joana Marinete de Souza", "Camareira", "Licença Maternidade", "07:00:00", "15:00:00", 2900),
+    (11, "Saulo Prestes Junior", "Camareiro", "Ativo", "07:00:00", "15:00:00", 2900),
+    (12, "Julyanna Freitas de Vasoncelos", "Camareira", "Ativo", "07:00:00", "15:00:00", 2900),
+    (13, "Ulisses Pietro Nunes", "Camareiro", "Ativo", "07:00:00", "15:00:00", 2900),
+    (14, "Marcela Moreira", "Camareira", "Temporário", "07:00:00", "15:00:00", 2700),
+    (15, "Marlene Pinha", "Camareira", "Ativo", "15:00:00", "23:00:00", 2900),
+    (16, "Geane Gertrudes", "Camareira", "Ativo", "15:00:00", "23:00:00", 2900),
+    (17, "Paulo de Tarso Silve", "Camareiro", "Ativo", "15:00:00", "23:00:00", 2900),
+    (18, "Lucas Yuri Fleh", "Camareiro", "Ativo", "15:00:00", "23:00:00", 2900),
+
+    (19, "Felipe Margueto", "Segurança", "Ativo", "06:00:00", "17:00:00", 4000),
+    (20, "Juan Carlos Danesi", "Segurança", "Licença Paternidade", "06:00:00", "17:00:00", 4000),
+    (21, "Layla Maria Fraccaro", "Segurança", "Ativo", "17:00:00", "02:00:00", 5000),
+    (22, "Salvatino Huihgh", "Segurança", "Temporário", "06:00:00", "17:00:00", 4000),
+    (23, "Lucas Marochevic de Toledo", "Segurança", "Ativo", "17:00:00", "02:00:00", 5000),
+    (24, "Ricardo Marola", "Segurança", "Ativo", "02:00:00", "07:00:00", 4000),
+    (25, "Tânia Mara da Silveira", "Segurança", "Ativo", "02:00:00", "07:00:00", 4000),
+
+    (26, "Sônia Staron", "Cozinheira", "Ativo", "06:00:00", "14:00:00", 4000),
+    (27, "Maria Rita Lucheve", "Cozinheira", "Ativo", "06:00:00", "14:00:00", 4000),
+    (28, "Gary Onus Burs", "Cozinheiro", "Ativo", "14:00:00", "22:00:00", 4000),
+    (29, "Alisson Gerber", "Cozinheiro", "Ativo", "14:00:00", "22:00:00", 4000),
+    (30, "Salete Meira", "Cozinheira", "Ativo", "22:00:00", "06:00:00", 4000),
+
+    (31, "Bruno Baldo", "Auxiliar de Cozinha", "Ativo", "06:00:00", "14:00:00", 2500),
+    (32, "Aristides Falave", "Auxiliar de Cozinha", "Férias", "06:00:00", "14:00:00", 2500),
+    (33, "Sonia Abraão Defuntosa", "Auxiliar de Cozinha", "Temporário", "06:00:00", "14:00:00", 2500),
+    (34, "Luiza Sonza", "Auxiliar de Cozinha", "Ativo", "14:00:00", "22:00:00", 2500),
+    (35, "Priscilla Alburdia", "Auxiliar de Cozinha", "Ativo", "14:00:00", "22:00:00", 2500),
+    (36, "Antony Salazar Matheus Juanil", "Auxiliar de Cozinha", "Ativo", "22:00:00", "06:00:00", 2500),
+    (37, "Carolina Anjos", "Auxiliar de Cozinha", "Ativo", "22:00:00", "06:00:00", 2500)    
 `
 function criaTabelaFuncionarios() {
     db.run(FUNCIONARIOS_SCHEMA, (error) => {
@@ -291,11 +330,11 @@ function populaTabelaFuncionarios() {
 db.serialize(() => {
     criaTabelaHospedes();
     populaTabelaHospedes();
-    criaTabelaQuartos();
-    populaTabelaQuartos();
+    // criaTabelaQuartos();
+    // populaTabelaQuartos();
     // criaTabelaReservas();
     // populaTabelaReservas();
-    criaTabelaFuncionarios ();
-    populaTabelaFuncionarios ();
+    // criaTabelaFuncionarios ();
+    // populaTabelaFuncionarios ();
 
 });
