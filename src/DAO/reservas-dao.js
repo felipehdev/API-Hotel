e28847878b31f4020053753574c946a2072e82c9
nahlavalentina <nahlavalentinas@gmail.com>
2022-08-09 15:03:57 -0300

    changed listarReservar to get guest id

--- src/DAO/reservas-dao.js
+++ src/DAO/reservas-dao.js
@@ -3,47 +3,51 @@ class ReservaDAO {
         this.bd = bd;
     }
 
-    inserirReserva(novaReserva){
+    inserirReserva(novaReserva) {
         return new Promise((resolve, reject) => {
-            this.bd.run(`INSERT INTO RESERVAS (ID_QUARTO, ID_HOSPEDE, CHECKIN, CHECKOUT, STATUS_PAGAMENTO) VALUES (?,?,?,?,?)`, 
-            [novaReserva.id_quarto, novaReserva.id_hospede, novaReserva.checkin, novaReserva.checkout, novaReserva.status_pagamento],
-            (erro) => {
-                if (erro) {
-                    console.log('reject');
-                    reject('Reserva não pode ser inserido', erro)
-                } else {
-                    console.log('resolve');
-                    resolve('Reserva inserida com sucesso')
-                }
-            })
+            this.bd.run(`INSERT INTO RESERVAS (ID_QUARTO, ID_HOSPEDE, CHECKIN, CHECKOUT, STATUS_PAGAMENTO) VALUES (?,?,?,?,?)`,
+                [novaReserva.id_quarto, novaReserva.id_hospede, novaReserva.checkin, novaReserva.checkout, novaReserva.status_pagamento],
+                (erro) => {
+                    if (erro) {
+                        console.log('reject');
+                        reject('Reserva não pode ser inserido', erro)
+                    } else {
+                        console.log('resolve');
+                        resolve('Reserva inserida com sucesso')
+                    }
+                })
         })
     }
 
-    listarReservas(){
+    listarReservas() {
         return new Promise((resolve, reject) => {
             this.bd.all(`SELECT * FROM RESERVAS`, (erro, resultado) => {
                 if (erro) {
                     reject("Erro ao selecionar o banco", erro)
                 } else {
-                    resolve({"Reservas selecionadas": resultado});
+                    resolve({
+                        "Reservas selecionadas": resultado
+                    });
                 }
             })
         })
     }
 
-    listarReserva(id){
+    listarReserva(id) {
         return new Promise((resolve, reject) => {
-            this.bd.all(`SELECT * FROM RESERVAS WHERE ID_RESERVA = ?`, [id], (erro, rows) => {
+            this.bd.all(`SELECT * FROM RESERVAS WHERE ID_HOSPEDE = ?`, [id], (erro, rows) => {
                 if (erro) {
                     reject("Erro ao selecionar o banco", erro)
                 } else {
-                    resolve({"Reserva selecionada": rows})
+                    resolve({
+                        "Reserva selecionada": rows
+                    })
                 }
             })
         })
     }
 
-    alterarReserva(novaReserva){
+    alterarReserva(novaReserva) {
         return new Promise((resolve, reject) => {
             this.bd.run(`UPDATE RESERVAS SET ID_QUARTO = ?, ID_HOSPEDE = ?, CHECKIN = ?, CHECKOUT = ?, STATUS_PAGAMENTO = ? WHERE ID_RESERVA = ?`, novaReserva, (erro) => {
                 if (erro) {
@@ -54,8 +58,8 @@ class ReservaDAO {
             })
         })
     }
-    deletarReserva(id){
-        return new Promise ((resolve, reject) => {
+    deletarReserva(id) {
+        return new Promise((resolve, reject) => {
             this.bd.run(`DELETE FROM RESERVAS WHERE ID_RESERVA = ${id}`, (erro) => {
                 if (erro) {
                     reject("Erro ao deletar a reserva", erro)
@@ -67,4 +71,6 @@ class ReservaDAO {
     }
 }
 
-export {ReservaDAO}
+export {
+    ReservaDAO
+}
